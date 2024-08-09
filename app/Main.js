7b516994acf7302085078bb00797ddb93fc6fdfa
yishai <yishai5555@gmail.com>
2024-08-09 17:18:46 +0300

    06-immer-fix-2

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-// import React, { useState, useReducer } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { useImmerReducer } from 'use-immer';
 
--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Page from './Page';
 import Axios from 'axios';
